fa9a823199628fdbce305ca7179f8c7cd5d0d548
reshetnikd <dmitry.reshetnik@icloud.com>
2020-03-12 11:42:11 +0200

    Initial Commit

---
+++ WhitehousePetitions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhitehousePetitions
+//
+//  Created by Dmitry Reshetnik on 12.03.2020.
+//  Copyright © 2020 Dmitry Reshetnik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
